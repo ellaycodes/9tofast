3dc507819034abe039db0ec92773768f5d012faa
Esther <11yekinie@gmail.com>
2025-10-27 08:00:05 +0000

    ensure expoConfifg exists

--- firebase/app.js
+++ firebase/app.js
@@ -7,12 +7,31 @@ import {
   getReactNativePersistence,
 } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Updates from "expo-updates";
+
+const extra =
+  Constants.expoConfig?.extra ??
+  Constants.manifest?.extra ??
+  Updates.manifest?.extra ??
+  {};
 
 const firebaseConfig = {
-  apiKey: Constants.expoConfig.extra.firebaseApiKey,
-  authDomain: Constants.expoConfig.extra.firebaseAuthDomain,
-  projectId: Constants.expoConfig.extra.firebaseProjectId,
+  apiKey: extra?.firebaseApiKey,
+  authDomain: extra?.firebaseAuthDomain,
+  projectId: extra?.firebaseProjectId,
 };
+
+const missingConfigKeys = Object.entries(firebaseConfig)
+  .filter(([, value]) => !value)
+  .map(([key]) => key);
+
+if (missingConfigKeys.length) {
+  const message = `Missing Firebase configuration values for: ${missingConfigKeys.join(
+    ", "
+  )}`;
+  console.error(message);
+  throw new Error(message);
+}
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
